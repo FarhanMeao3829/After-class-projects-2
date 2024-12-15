910264ad46c75ffef0a015361d5546c598603d5e
Farhan Shafiq Chowdhury <yfarhana044@gmail.com>
2024-12-15 09:29:49 +0600

    first commit

---
+++ visualisation using matplotlib.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train = pd.read_csv("company_sales_data.csv")
+print(train)
+
+profitList = train ['totalProfit'].tolist()
+monthList = train ['monthNumber'].tolist()
+
+plt.plot(monthList, profitList, label= 'Month-wise profit data of the last year')
+
+plt.xlabel('Month number')
+plt.ylabel('Profit in dollar')
+plt.xticks(monthList)
+plt.title('Company profit per month')
+plt.yticks([100000, 300000, 400000, 600000, 800000])
+plt.show()
+
+profitList = train ['totalProfit'].tolist()
+monthList = train ['monthNumber'].tolist()
+
+plt.plot(monthList, profitList, label= 'Profit data of last year',
+         color='r', marker='o', markerfacecolor='k',
+         linestyle='--', linewidth=3)
+
+plt.xlabel('Month Numbr')
+plt.ylabel('Profit in dollar')
+plt.legend(loc= 'lower right')
+plt.title('Company sales data of the last year')
+plt.xticks(monthList)
+plt.yticks([100000, 300000, 400000, 600000, 800000])
+plt.show()
